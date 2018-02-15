872cc5f1b8349720f465c288f5c3b901aa9084f6
EngelschallNikolaus <31962790+EngelschallNikolaus@users.noreply.github.com>
2018-02-15 15:10:06 +0100

    solution of homework

---
+++ solution/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  startValue: number;
+  height: number;
+  tower: string[] = [];
+
+  calculate() {
+    this.tower=[];
+    if (this.height >= 2 && this.startValue !== undefined && this.startValue !== null) {
+      let currVal = this.startValue;
+      for (let i = 2; i <= this.height; i++) {
+        this.tower.push(currVal + " * " + i + " = " + currVal * i);
+        currVal = currVal * i;
+      }
+      for (let i = 2; i <= this.height; i++) {
+        this.tower.push(currVal + " / " + i + " = " + currVal / i);
+        currVal = currVal / i;
+      }
+    }
+  }
+}
+

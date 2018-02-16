cddf9158f74f060ec114fa94b15031bcd60a4fc5
EngelschallNikolaus <31962790+EngelschallNikolaus@users.noreply.github.com>
2018-02-16 08:58:03 +0100

    changes done

--- solution/app.component.ts
+++ solution/app.component.ts
@@ -8,19 +8,25 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   startValue: number;
-  height: number;
-  tower: string[] = [];
+  height: number = 0;
+  tower: string[][] = [];
 
   calculate() {
-    this.tower=[];
+    this.tower = [];
     if (this.height >= 2 && this.startValue !== undefined && this.startValue !== null) {
       let currVal = this.startValue;
       for (let i = 2; i <= this.height; i++) {
-        this.tower.push(currVal + " * " + i + " = " + currVal * i);
+        this.tower[i - 2] = [];
+        this.tower[i - 2].push(currVal + " * " + i);
+        this.tower[i - 2].push(" = ");
+        this.tower[i - 2].push(currVal * i + "");
         currVal = currVal * i;
       }
       for (let i = 2; i <= this.height; i++) {
-        this.tower.push(currVal + " / " + i + " = " + currVal / i);
+        this.tower[i+this.height - 2] = [];
+        this.tower[i+this.height - 2].push(currVal + " / " + i);
+        this.tower[i+this.height - 2].push(" = ");
+        this.tower[i+this.height - 2].push(currVal / i + "");
         currVal = currVal / i;
       }
     }
